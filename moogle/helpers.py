3cce50a0157e78616a767db82266f03bde828b38
Michael Onjack <mikeonjack@gmail.com>
2016-05-10 06:03:40 -0400

    delete

--- moogle/helpers.py
+++
@@ -1,8 +0,0 @@
-def get_config():
-	''' Create a config parser for reading INI files '''
-	try:
-		import ConfigParser
-		return ConfigParser.ConfigParser()
-	except:
-		import configparser
-		return configparser.ConfigParser()
