d732b13f181ac13fda93b698657fb7b7c6e2034d
Michael Onjack <mikeonjack@gmail.com>
2016-05-13 21:06:36 -0400

    Update auction page to add countdown timer

--- moogle/js/timer.js
+++ moogle/js/timer.js
@@ -37,5 +37,6 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+var deadline = new Date(Date.parse(document.getElementById("enddate").value));
 initializeClock('clockdiv', deadline);
+
